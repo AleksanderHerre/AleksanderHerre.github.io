83d0b18ec9d048e686389900f50c989583485b57
AleksanderHerre <aleksherred1@hotmail.com>
2024-05-31 11:03:13 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -9,20 +9,23 @@ $password = "passord";
 
 // Connect to the database
 $conn = new mysqli($servername, $username, $password, $database);
-echo"connected ";
+echo" connected ";
 
 
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo " test";
+echo " test ";
 
 $UserOrEmail = $conn->real_escape_string($_POST['UserOrEmail']);
 $LoginPassword = $conn->real_escape_string($_POST['LoginPassword']);
 
 $LoginEmailOrUser = "SELECT EmailAdress, Username FROM user WHERE EmailAdress = '$UserOrEmail' OR Username = '$UserOrEmail'";
 $LoginCheckPassword = "SELECT Password FROM user WHERE Password = '$LoginPassword'";
-echo "woho!"
+
+echo "woho" ;
+
+
 $conn->close();
 ?>
