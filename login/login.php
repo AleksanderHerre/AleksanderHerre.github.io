3cb8d820c9de24cd9c7f40730a8fa4c490ecbf2f
AleksanderHerre <aleksherred1@hotmail.com>
2024-06-05 10:17:58 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -41,7 +41,7 @@ echo "2";
 
 $LoginCheckPassword = "SELECT Password FROM user WHERE Password = '$LoginPassword'";
 $stmt = $conn->prepare($LoginCheckPassword);
-$stmt->bind_param("s", $LoginPassword);
+$stmt->bind_param("s", Password);
 echo"2.5";
 $stmt->execute();
 $result = $stmt->get_result();
