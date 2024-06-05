a4ead8585f0f4ac6cd8356f925db2ca9e54d0947
AleksanderHerre <aleksherred1@hotmail.com>
2024-06-05 10:12:48 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -39,7 +39,7 @@ if ($uResult->num_rows == 0) {
 
 echo "2";
 
-$LoginCheckPassword = "SELECT Password FROM user WHERE Password = ?";
+$LoginCheckPassword = "SELECT Password FROM user WHERE Password = 'Password'";
 echo"2.5";
 $stmt = $conn->prepare($LoginCheckPassword);
 $stmt->bind_param("s", $LoginPassword);
