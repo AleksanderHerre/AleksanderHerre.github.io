3e47bf195a6d4d7e65be09f671c8ac2ac6adf196
AleksanderHerre <aleksherred1@hotmail.com>
2024-06-06 09:02:41 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -21,24 +21,14 @@ echo "1";
 $UserOrEmail = $conn->real_escape_string($_POST['UserOrEmail']);
 $LoginPassword = $conn->real_escape_string($_POST['LoginPassword']);
 
-$LoginEmailOrUser = "SELECT EmailAdress, Username FROM user WHERE EmailAdress = ? OR Username = ?";
+$LoginEmailOrUser = "SELECT EmailAdress, Username, Password FROM user WHERE EmailAdress = ? OR Username = ?";
 $uStmt = $conn->prepare($LoginEmailOrUser);
 $uStmt->bind_param("ss", $UserOrEmail, $UserOrEmail);
 $uStmt->execute();
 $uResult = $uStmt->get_result();
 
-$LoginCheckPassword = "SELECT Password FROM user WHERE Password = ? ";
-echo"2.5";
-$pStmt = $conn->prepare($LoginCheckPassword);
-echo"2.6";
-$pStmt->bind_param("s", $LoginPassword);
 
-$pStmt->execute();
-echo"2.7";
-$pResult = $pStmt->get_result();
-echo "3";
-
-$user = $pResult->fetch_assoc();
+$user = $uResult->fetch_assoc();
 if (!$user || !password_verify($LoginPassword, $user['Password'])) {
     header("Location: ../index.html"); // Redirect to index.html
     exit;
