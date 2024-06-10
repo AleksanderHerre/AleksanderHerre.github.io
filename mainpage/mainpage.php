a6cbb71a4acf00eb18c7a480d899984d15cb0c33
AleksanderHerre <aleksherred1@hotmail.com>
2024-06-10 10:13:53 +0200

    Update mainpage.php

--- mainpage/mainpage.php
+++ mainpage/mainpage.php
@@ -19,7 +19,7 @@ include '../menu/connect.php';
         <!-- Bakgrunns bilde -->
         <img id="img" class="banner" src="https://www.bm-t.no/wp-content/uploads/2021/11/gandvgs2.jpg" alt="">
         </div>
-        <h1 id="hei" class="hidden">
+        <h1 id="hei">
             Hei, <?php ($_SESSION['Username']); ?> velkommen til mitt nettsted IT VG2 oppgave.
         </h1>
     </main>
