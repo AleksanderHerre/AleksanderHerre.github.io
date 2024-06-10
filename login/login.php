f82bfc7a661a6dbdd7b50585e1c6ea3b982035f4
AleksanderHerre <aleksherred1@hotmail.com>
2024-06-11 00:02:34 +0200

    Fjernet mellomrom :fire:
    Fjernet mellomrom for renhet.

--- login/login.php
+++ login/login.php
@@ -11,7 +11,7 @@ $password = "passord";
 $conn = new mysqli($servername, $username, $password, $database);
 
 // Check the connection
-if ($conn->connect_error) {
+if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
