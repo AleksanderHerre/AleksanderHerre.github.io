5a246b1a3226956bc7a1b08c54e3b4b0e487afb1
AleksanderHerre <aleksherred1@hotmail.com>
2024-06-10 13:13:22 +0200

    Update signup.php

--- login/signup.php
+++ login/signup.php
@@ -36,7 +36,7 @@ if ($UsernameResult->num_rows != 0 and $MailResult ->num_rows != 0){
 	die( "Dette er allerede en eksisterende mail" )	;	
 } elseif ($UsernameResult-> num_rows != 0)
 	die( "Dette brukernavnet er allerede i bruk") ;
- else {
+else {
 	$passwordHashed = password_hash($password, PASSWORD_BCRYPT);
 	// Lager en SQL setning som settes inn som brukerdata i databasen 
 	$sql = "INSERT INTO user (EmailAdress, Username, Password) VALUES ('$mail', '$username', '$passwordHashed');";
