b91b48252ed99c8b6b359fc49e1bd18f9366f86e
AleksanderHerre <aleksherred1@hotmail.com>
2024-06-11 04:24:37 +0200

    Update login.js

--- login/login.js
+++ login/login.js
@@ -32,17 +32,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const usernameInput = createAccountForm.querySelector("input[name='username']");
         const passwordInput = createAccountForm.querySelector("input[name='password']");
+        const mailInput = createAccountForm.querySelector("input[name='mail']");
         
         const username = usernameInput.value;
         const password = passwordInput.value;
+        const mail = mailInput.value;
 
         const usernamePattern = /^(?=.{3,})/;
         const passwordPattern = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/;
+        const mailPattern = /^(?=.*[@.])/;
 
         let isValid = true;
 
+        if (!mailPattern.test(mail)) {
+            setInputError(mailInput, "Email skal inneholde @ og .");
+            isValid = false; 
+        } else {
+            clearInputError(mailInput);
+        }
+
         if (!usernamePattern.test(username)) {
-            setInputError(usernameInput, "Username skal inneholde flere en 3 karakterer og skal inneholde minst en stor bokstav og spesiell karakter.");
+            setInputError(usernameInput, "Brukernavnet skal inneholde 3 eller flere karakterer.");
             isValid = false;
         } else {
             clearInputError(usernameInput);
